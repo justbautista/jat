fc83abf9f3728921250e0cbf3e9c703d97c5456f
jamessciacca <noing2003@gmail.com>
2023-10-07 17:14:11 -0400

    added text to home

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
     <footer className="bg-slate-600 text-black fixed bottom-0 w-full p-4">
       <div className="container mx-auto text-center">
         <p>&copy; JAT Group 2023</p>
+        <a href="https://github.com/justbautista/jat">Github</a>
       </div>
     </footer>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,11 @@ function Home(){
     return(
     <>
         <NavBar />
+        <div className="container mx-auto text-center">
+            <h1 className="text-4xl pt-32 pb-4">Applying to Jobs, made fun!</h1>
+            <p className="text-sm border-solid"> JAT allows users to not only keep track of their job applications, but encourages them to apply to more job oppurtunities as well. Earn badges and keep your streak going day to day as you look for your new job oppurtunities.</p>
+        </div>
+        
         <Footer />
     </>
     )
