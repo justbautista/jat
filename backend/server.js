a54c6ee490101b29968ca249cf3ab3fe56b26b82
Nicky Chen <nickychen2022@gmail.com>
2023-10-07 13:54:48 -0400

    testing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-// First backend commit 
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => res.send("API running"));
+app.listen(port, () => console.log(`Server Started on Port:${port}`));
