75adc5d5ca8bbc3fcb9201620c640d48166eaf50
justin <justinbautista923@gmail.com>
2023-10-07 17:11:45 -0400

    merged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import Home from "./pages/Home.jsx"
 import { useAuth0 } from "@auth0/auth0-react"
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import PrivateRoutes from "./components/PrivateRoutes.jsx"
 import Dashboard from "./pages/Dashboard.jsx"
-=======
-
-import Home from "./pages/Home.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
->>>>>>> refs/remotes/origin/frontend
 
 function App() {
 	const { isAuthenticated } = useAuth0()
