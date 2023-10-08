9299ea1b3e84ff42fd25ba97ed98ca5465a3ff20
justin <justinbautista923@gmail.com>
2023-10-08 09:54:39 -0400

    check

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,7 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react"
+import { useEffect } from "react"
 
 export default function LoginButton() {
 	const { loginWithRedirect } = useAuth0()
+
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+            loginWithRedirect()
+        }
+    }, [])
 
 	return (
 		<>
