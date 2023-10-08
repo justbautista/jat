dc07c415a66273ee89e6304cd9bef81e496a949c
justin <justinbautista923@gmail.com>
2023-10-07 23:05:16 -0400

    testing

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,36 +1,34 @@
 import NavBar from "../components/NavBar"
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import api from "../helpers/axiosConfig"
 import SideBar from "../components/SideBar"
 import { useAuth0 } from "@auth0/auth0-react"
 import JobList from "../components/JobList"
 
 function Dashboard() {
-    const { user } = useAuth0()
-    const [jobsApplied, setJobsApplied] = useState()
+	const { user } = useAuth0()
+	const [jobsApplied, setJobsApplied] = useState({})
 
-    useEffect(() => {
-        // const getJobsApplied = async () => {
-        //     try {
-        //         const response = await api.post("/api/users/jobs", {
-        //             email: user.email
-        //         })
-        //         setJobsApplied(response.data)
-        //     } catch (err) {
-        //         console.error(err)
-        //     }
-        // }
+	useEffect(() => {
+		const getJobsApplied = async () => {
+		    try {
+		        const response = await api.post("/api/users/jobs", {
+		            email: user.email
+		        })
+		        setJobsApplied(response.data)
+		    } catch (err) {
+		        console.error(err)
+		    }
+		}
 
-        // getJobsApplied()
-        const test = "diasodj"
-        setJobsApplied(test)
-        console.log(jobsApplied)
-    }, [user])
+		getJobsApplied()
+	}, [])
+
 	return (
 		<div>
 			<NavBar />
-			<SideBar jobsApplied={jobsApplied}/>
-            <JobList jobsApplied={jobsApplied}/>
+			<SideBar jobsApplied={jobsApplied} />
+			<JobList jobsApplied={jobsApplied} />
 		</div>
 	)
 }
