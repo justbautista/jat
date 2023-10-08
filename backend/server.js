ae1e1cfa9863ab3e8a56f4e3a0600283d613d9c4
jaivalbhuptani <jaivalbhuptani1406@gmail.com>
2023-10-07 21:07:04 -0400

    Synthesised data

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { connectDB } = require("./config/db");
+const jobRoutes = require("./routes/jobRoutes");
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 connectDB();
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/jobs", jobRoutes);
 app.get("/", (req, res) => res.send("API running"));
 app.listen(port, () => console.log(`Server Started on Port:${port}`));
